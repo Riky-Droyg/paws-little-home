37b63948fe59992716df8f0343120b2d74ce9b2b
Pavlo Hapeniuk <johnybfrank@gmail.com>
2025-12-13 23:27:39 +0200

    add webp img

---
+++ src/js/about-us.js
@@ -0,0 +1,12 @@
+// var swiper = new Swiper('.mySwiper', {
+//   spaceBetween: 30,
+//   effect: 'fade',
+//   navigation: {
+//     nextEl: '.about-us-swiper-button-next',
+//     prevEl: '.about-us-swiper-swiper-button-prev',
+//   },
+//   pagination: {
+//     el: '.about-us-swiper-pagination',
+//     clickable: true,
+//   },
+// });
