e53b75d30ac1899f9563ae1662258f270e55cb79
Riky-Droyg <riky.droyg@gmail.com>
2025-12-14 18:33:44 +0200

    added a new library for automatic sprite generation

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
-import "/js/header.js";
-import "/js/hero.js";
-import "/js/pets-list.js";
-import "/js/about-us.js";
-import "/js/faq.js";
-import "/js/success-stories.js";
-import "/js/animal-details.js";
-import "/js/order-modal.js";
-import "/js/footer.js";
-
-
-
+import 'virtual:svg-icons-register';
+import '/js/header.js';
+import '/js/hero.js';
+import '/js/pets-list.js';
+import '/js/about-us.js';
+import '/js/faq.js';
+import '/js/success-stories.js';
+import '/js/animal-details.js';
+import '/js/order-modal.js';
+import '/js/footer.js';
